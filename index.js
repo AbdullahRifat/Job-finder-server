68de3df8c9c69e8fa7b5ddf9b56b39ae08a9d266
AbdullahRifat <abdrifat69@gmail.com>
2023-11-08 11:13:21 +0600

    aded apply job functionality

--- index.js
+++ index.js
@@ -12,13 +12,35 @@ require('dotenv').config()
 
 app.use(cors(
    {
-    origin:'http://localhost:5173'
+    origin:['http://localhost:5173']
     ,
     credentials:true
    }
 ));
 app.use(express.json())
 app.use(cookieParser());
+
+//customs middlewire
+const logger = (req, res, next) => {
+  console.log('log: info', req.method, req.url);
+  next();
+}
+
+const verifyToken = (req, res, next) => {
+  const token = req?.cookies?.token;
+  // console.log('token in the middleware', token);
+  // no token available 
+  if (!token) {
+      return res.status(401).send({ message: 'unauthorized access' })
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+      if (err) {
+          return res.status(401).send({ message: 'unauthorized access' })
+      }
+      req.user = decoded;
+      next();
+  })
+}
 
 
 
@@ -47,9 +69,9 @@ async function run() {
     //auth
     app.post('/jwt',async(req, res)=>{
       const user = req.body;
-      console.log(user)
+    
 
-      const token = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{expiresIn:'1h'})
+      const token = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{expiresIn:'100h'})
       res
       .cookie('token',token,{
         httpOnly: true,
@@ -59,6 +81,11 @@ async function run() {
       })
       .send({success:true})
 
+  })
+  //clearing cookies
+  app.post('/logout',async(req,res)=>{
+      const user = req.body;
+      res.clearCookie('token',{maxAge:0}).send({success:true})
   })
 
 
@@ -83,9 +110,11 @@ async function run() {
       res.send(result) // Return the job data as JSON
     });
 
-    app.get("/jobdetails/:jobid", async (req, res) => {
+    app.get("/jobdetails/:jobid",verifyToken ,async (req, res) => {
+     
+
       const jobid = req.params.jobid;
-      console.log('token  ',req.cookies.token)
+      
       const filter = { _id: new ObjectId(jobid) }
      
       const job = await allJobs.findOne(filter);
@@ -97,7 +126,7 @@ async function run() {
     //posting jobs
     app.post('/alljobs',async (req,res)=>{
         const newjob= req.body;
-        console.log(newjob);
+     
         const result = await allJobs.insertOne(newjob)
         res.send(result);
 
@@ -131,29 +160,145 @@ async function run() {
     //   res.send(job) // Return the job data as JSON
     // });
 
-    app.put('/updatejob/:jobid',async (req,res)=>{
+    app.put('/updatejob/:jobid', verifyToken, async (req, res) => {
       const jobid = req.params.jobid;
-      console.log(jobid)
     
-      const filter = { _id: new ObjectId(jobid) }
-      console.log(filter)
-     const updatejob = req.body
-     
-     const updateDoc = {
-      $set: updatejob,
-    };
-
+      const filter = { _id: new ObjectId(jobid) };
+      const job = await allJobs.findOne(filter);
+    
+      if (!job) {
+        return res.status(404).send({ message: 'Job not found' });
+      }
+    
+      // Check if the user is authorized to update this job
+      if (req.user.email !== job.email) {
+        return res.status(403).send({ message: 'Forbidden access' });
+      }
+    
+      const updatejob = req.body;
+      const updateDoc = {
+        $set: updatejob,
+      };
+    
+    
       const options = { upsert: true };
-      
+    
       const result = await allJobs.updateOne(filter, updateDoc, options);
-      console.log(result)
+    
+      res.send(result);
+    });
+    
+    //
+    app.get('/updatejob/:jobid',logger, verifyToken, async (req, res) => {
+      const jobid = req.params.jobid;
+      const filter = { _id: new ObjectId(jobid) };
+      const job = await allJobs.findOne(filter);
+      console.log(req.user.email)
+    
+      if (!job) {
+        return res.status(404).send({ message: 'Job not found' });
+      }
+      console.log(req.user.email)
+      // Check if the user is authorized to update this job
+      
+      if (req.user.email !== job.email) {
+        return res.status(403).send({ message: 'Forbidden access' });
+      }
+    
+      res.send(job);
+    });
+
+    //delete
+
+    app.get('/myjobs',verifyToken,async(req,res)=>{
+
+
+
+      if(req?.user?.email !== req?.query?.email) {
+        console.log(req.user.email,req.params.email)
+        console.log("unnnnnnn")
+        return res.status(403).send({ message: 'Forbidden access' });
+      }
+      let query = {};
+
+
+      if(req.query?.email){
+        query = {email:req.query.email}
+      }
+      
+      const result = await allJobs.find(query).toArray()
       res.send(result)
 
     })
+    //applied jobs
+    app.get('/appliedjobs', verifyToken, async (req, res) => {
+      console.log(req.query.email)
+      if (req.user.email !== req.query.email) {
+        console.log(req.user.email, req.query.email);
+        console.log("unnnnnnn");
+        return res.status(403).send({ message: 'Forbidden access' });
+      }
+    
+      let query = {};
+    
+      if (req.query.email) {
+        query = { applyemail: { $elemMatch: { $eq: req.query.email } } };
+      }
+    
+      const result = await allJobs.find(query).toArray();
+      res.send(result);
+    });
 
-    //delete
-    app.delete('/delete/:jobid',async (req,res)=>{
+
+    //apply jobs
+app.post('/applyjob/:jobid',verifyToken, async (req, res) => {
+  const jobid = req.params.jobid;
+  const filter = { _id: new ObjectId(jobid) };
+  const job = await allJobs.findOne(filter);
+
+  if (!job) {
+    return res.status(404).send({ message: 'Job not found' });
+  }
+  console.log(req.user.email)
+  const user = req?.user;
+  const userEmail = user?.email;
+
+  // Check if the user has already applied for the job
+  if (job.applyemail.includes(userEmail)) {
+    return res.status(400).send({ message: 'You have already applied for this job.' });
+  }
+
+  // Update the applyemail array by pushing the user's email
+  job.applyemail.push(userEmail);
+
+  const updateData = {
+    jobApplicants: (parseInt(job.jobApplicants, 10) || 0) + 1,
+    applyemail: job.applyemail,
+  };
+
+  const updateDoc = {
+    $set: updateData,
+  };
+
+  const options = { upsert: true };
+
+  // Send a PUT request to update the job with the new application data
+  const result = await allJobs.updateOne(filter, updateDoc, options);
+
+  res.send(result);
+});
+
+
+
+
+
+    app.delete('/myjobs/:jobid',async (req,res)=>{
+
+      // if(req.user.email !== req.query.email){
+      //   return res.status(403).send({message: 'forbidden access'})
+      // }
       const jobid = req.params.jobid;
+      console.log(jobid)
       const query = {_id: new ObjectId(jobid)};
       const result = await allJobs.deleteOne(query)
       res.send(result);
